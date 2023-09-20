14a64f80a33d72d98705d707a0812b068826f2de
emadhegazy <acc.ekh1@gmail.com>
2023-09-20 15:55:18 -0700

    this function is to handle errors

--- errors_emad.c
+++ errors_emad.c
@@ -39,4 +39,31 @@ void more_err(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+/**
+ * string_err - this function is to handle errors.
+ * @error_code: The error codes are int type and are as the following:
+ * (10) ~> case 10 is the number inside a node is outside ASCII bounds.
+ * (11) ~> case 11 is the stack is empty.
+ */
+void string_err (int error_code, ...)
+{
+	va_list ag;
+	int l_num;
+
+	va_start(ag, error_code);
+	l_num = va_arg(ag, int);
+	switch (error_code)
+	{
+		case 10:
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", l_num);
+			break;
+		case 11:
+			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
 
