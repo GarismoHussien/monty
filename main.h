9a468d96fa1a24bf49f763fd882e5478ad36ab19
GarismoHussien <hand2land@gmail.com>
2023-09-20 16:14:48 -0700

    add main.h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
