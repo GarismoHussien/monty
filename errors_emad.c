b885ffd220abf6425e528bcc65f4d5e24b555cbe
emadhegazy <acc.ekh1@gmail.com>
2023-09-20 16:02:45 -0700

    errors_emad.c

--- errors_emad.c
+++ errors_emad.c
@@ -45,7 +45,7 @@ void more_err(int error_code, ...)
  * (10) ~> case 10 is the number inside a node is outside ASCII bounds.
  * (11) ~> case 11 is the stack is empty.
  */
-void string_err (int error_code, ...)
+void string_err(int error_code, ...)
 {
 	va_list ag;
 	int l_num;
@@ -59,6 +59,54 @@ void string_err (int error_code, ...)
 			break;
 		case 11:
 			fprintf(stderr, "L%d: can't pchar, stack empty\n", l_num);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+/**
+ * err - this is to Print the error messages determined by their error code.
+ * @error_code: The error codes is int type are the following:
+ * (1) => first error is the user does not give any file
+ * or more than one file to the program.
+ * (2) => second error is the file provided is not
+ * a file that can be opened or read.
+ * (3) => third error is the file provided contains an invalid instruction.
+ * (4) => fourth error is when the program is unable to malloc more memory.
+ * (5) => fifth error is when the parameter passed
+ * to the instruction "push" is not an int.
+ * (6) => sixth error is when the stack it empty for pint.
+ * (7) => seventh error is when the stack it empty for pop.
+ * (8) => eighth error is when stack is too short for operation.
+ */
+void err(int error_code, ...)
+{
+	va_list ag;
+	char *opp;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			fprintf(stderr, "USAGE: monty file\n");
+			break;
+		case 2:
+			fprintf(stderr, "Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			opp = va_arg(ag, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, opp);
+			break;
+		case 4:
+			fprintf(stderr, "Error: malloc failed\n");
+			break;
+		case 5:
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
 			break;
 		default:
 			break;
