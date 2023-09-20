ec07bb4f2ef9e3930bfae097d88f38d2247f5354
GarismoHussien <hand2land@gmail.com>
2023-09-20 16:21:29 -0700

    stack func2

--- stack_func2_ahmed.c
+++ stack_func2_ahmed.c
@@ -19,4 +19,78 @@ void swap_nodes(stack_t **stack, unsigned int line_number)
 	tmp->prev = NULL;
 	*stack = tmp;
 }
+/**
+ * nop - this function is to Do nothing.
+ * @stack: this is a Pointer that  pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
+
+
+/**
+ * add_nodes - this function is to add the top two elements of the stack.
+ * @stack: ti is a pointer Pointer to that pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void add_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "add");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+
+
+
+/**
+ * div_nodes - this is to add the top two elements of the stack.
+ * @stack: this is a pointer that  pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void div_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "div");
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
+ * sub_nodes - this is to add the top two elements of the stack.
+ * @stack: this is a Pointer that pointing to top node of the stack.
+ * @line_number: thisis int type and  representing the line number of of the opcode.
+ */
+void sub_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "sub");
+
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
 
