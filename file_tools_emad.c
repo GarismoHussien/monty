30cc2efbe953c7194bb610efe5a8336a453e6791
emadhegazy <acc.ekh1@gmail.com>
2023-09-20 16:09:20 -0700

    this function is to find the appropriate function for the opcode

--- file_tools_emad.c
+++ file_tools_emad.c
@@ -48,4 +48,51 @@ int parse_line(char *buffer, int line_number, int format)
 	find_func(opcode, value, line_number, format);
 	return (format);
 }
+/**
+ * find_func - this function is to find the appropriate function for the opcode
+ * @opcode: this is a pointer opcode
+ * @value: this is a pointer char type argument of opcode
+ * @format:  thisis int type storage format. If 0 Nodes will be entered as a stack.
+ * @ln: this is int type and line number
+ * if 1 node will be entered as a queue.
+ * Return: always return void
+ */
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int i;
+	int flag;
+
+	instruction_t func_list[] = {
+		{"push", add_to_stack},
+		{"pall", print_stack},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"nop", nop},
+		{"swap", swap_nodes},
+		{"add", add_nodes},
+		{"sub", sub_nodes},
+		{"div", div_nodes},
+		{"mul", mul_nodes},
+		{"mod", mod_nodes},
+		{"pchar", print_char},
+		{"pstr", print_str},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+	};
+
+	if (opcode[0] == '#')
+		return;
+
+	for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, func_list[i].opcode) == 0)
+		{
+			call_fun(func_list[i].f, opcode, value, ln, format);
+			flag = 0;
+		}
+	}
+	if (flag == 1)
+		err(3, ln, opcode);
+}
 
