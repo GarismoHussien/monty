d7381873ad9d63cd15ab776c78419c276a242612
emadhegazy <acc.ekh1@gmail.com>
2023-09-21 01:35:02 -0700

    this is to add the top two elements of the stack

--- stack_func2_ahmed.c
+++ stack_func2_ahmed.c
@@ -31,4 +31,44 @@ void add_nodes(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+/**
+ * div_nodes - this is to add the top two elements of the stack.
+ * @stack: this is a pointer that  pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void div_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "div");
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n / (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
+ * sub_nodes - this is to add the top two elements of the stack.
+ * @stack: this is a Pointer that pointing to top node of the stack.
+ * @line_number: thisis int type and  representing the line number of of the opcode.
+ */
+void sub_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "sub");
+
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
 
