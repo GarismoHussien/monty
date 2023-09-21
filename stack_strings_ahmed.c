e7c83341f4f08138ebc07e7306708ba8e134e14b
emadhegazy <acc.ekh1@gmail.com>
2023-09-21 01:28:44 -0700

    this function is to print the Ascii value

--- stack_strings_ahmed.c
+++ stack_strings_ahmed.c
@@ -49,4 +49,44 @@ void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
 	(*stack)->prev->next = NULL;
 	(*stack)->prev = NULL;
 }
+/**
+ * rotr - this function is to Rotate the last node of the stack to the top.
+ * @stack: this is a pointer that it to point to the top node of the stack.
+ * @ln: this is int type and  representing the line number of of the opcode.
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
+/**
+ * print_char - this function is to print the Ascii value.
+ * @stack: this is a Pointer that pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_number);
+
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+	printf("%c\n", ascii);
+}
 
