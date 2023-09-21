7d5d9d80e8ca5636af3f9d45471ce96cbe5a85ab
emadhegazy <acc.ekh1@gmail.com>
2023-09-21 00:53:09 -0700

    this function is to add a node to the stack

--- stack_func1_emad.c
+++ stack_func1_emad.c
@@ -48,4 +48,25 @@ void print_top(stack_t **stack, unsigned int line_number)
 		more_err(6, line_number);
 	printf("%d\n", (*stack)->n);
 }
+/**
+ * add_to_stack - this function is to add a node to the stack.
+ * @new_node: this is a pointer that points to the new node.
+ * @ln: Interger thisis int type and representing the line number of of the opcode.
+ */
+void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	head = *new_node;
+	head->next = tmp;
+	tmp->prev = head;
+}
 
