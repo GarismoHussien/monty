0b0d1fb2548796e9abcbac7a2486c771ea3c5f9e
emadhegazy <acc.ekh1@gmail.com>
2023-09-21 01:31:37 -0700

    this is to add the top two elements of the stack

--- stack_op_ahmed.c
+++ stack_op_ahmed.c
@@ -22,5 +22,23 @@ void mod_nodes(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+/**
+ * mul_nodes - this is to add the top two elements of the stack.
+ * @stack: this is a Pointer that  pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void mul_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "mul");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
 
 
