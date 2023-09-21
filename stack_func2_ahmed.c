d2d5cd48e3c9af63811f3afb91e0a02ade9ec29f
emadhegazy <acc.ekh1@gmail.com>
2023-09-21 01:36:09 -0700

    this function is to swap the top two elements of the stack

--- stack_func2_ahmed.c
+++ stack_func2_ahmed.c
@@ -71,4 +71,24 @@ void sub_nodes(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+/**
+ * swap_nodes - thisfunction is to swap the top two elements of the stack.
+ * @stack: this is a Pointer that pointing to top node of the stack.
+ * @line_number: this is int type and  representing the line number of of the opcode.
+ */
+void swap_nodes(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "swap");
+	tmp = (*stack)->next;
+	(*stack)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *stack;
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	tmp->prev = NULL;
+	*stack = tmp;
+}
 
